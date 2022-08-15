c14c9d5f65de59374eb31593309a6a6e13edfbce
HannachiHassen <hannachihassen07@gmail.com>
2022-08-15 14:24:40 -0400

    Create 'AccountPage' class in pageObject package

---
+++ src/main/java/pageObject/AccountPage.java
@@ -0,0 +1,21 @@
+package pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import resources.Base;
+
+public class AccountPage extends Base{
+	
+	public AccountPage() {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//a[contains(text(),'Edit your account information')]")
+	WebElement editAccountLink;
+	
+	public WebElement editAccountInformation() {
+		return editAccountLink;		
+	}
+}
--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -2,6 +2,7 @@ package testCases;
 
 import org.testng.annotations.Test;
 
+import pageObject.AccountPage;
 import pageObject.LandingPage;
 import pageObject.LoginPage;
 import resources.Base;
@@ -25,5 +26,8 @@ public class LoginTest extends Base{
 		loginPage.email().sendKeys("arun.selenium@gmail.com");
 		loginPage.password().sendKeys("Second@123");
 		loginPage.clickLoginbtn().click();
+		
+		AccountPage account=new AccountPage();
+		account.editAccountInformation().click();
 	}	
 }
